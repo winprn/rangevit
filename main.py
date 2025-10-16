f7d2fc03e21bc5cd700b9bb54891323d9d5bd499
KietHuynh <kiet.huynh2@orientsoftware.com>
2025-10-16 19:37:01 +0700

    Revert "Merge branch 'main' into newbaseline"
    This reverts commit 8a6ca0d0df3b9916390a351dca03abd4daadb87d, reversing
    changes made to aa4622cf290d307d10685d09a995a20fea04fefe.

--- main.py
+++ main.py
@@ -175,10 +175,9 @@ class Experiment(object):
         self.mlflow_active = mlflow_active
 
         # Init gpu
-
-        if tools.is_dist_avail_and_initialized():
-            tools.init_distributed_mode(self.settings)
-        # torch.distributed.barrier()
+        tools.init_distributed_mode(self.settings)
+        if torch.cuda.is_available() and self.settings.distributed:
+            torch.distributed.barrier()
 
         self.settings.check_path()
 
@@ -189,10 +188,6 @@ class Experiment(object):
             torch.cuda.set_device(self.settings.gpu)
             torch.backends.cudnn.benchmark = True
         np.random.seed(self.settings.seed)
-        # torch.cuda.set_device(self.settings.gpu)
-        # torch.cuda.set_device("cuda:0")
-
-        torch.backends.cudnn.benchmark = True
 
         # Init checkpoint
         self.recorder = None
@@ -552,7 +547,7 @@ if __name__ == '__main__':
                         help='path to save the file, type: string')
     parser.add_argument('--id', type=str,
                         help='name to identify the run')
-    parser.add_argument('--num_workers', type=int, default=2,
+    parser.add_argument('--num_workers', type=int, default=4,
                         help='number of threads used for data loading, type: int')
     parser.add_argument('--pretrained_model', type=str,
                         help='path of pre-trained model to initialize the ViT encoder backbone, type: string')
